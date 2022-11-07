d1dd83105dfd509c2355cdebbfa980f14368e498
swagvillain <marshall.taylor.brown@gmail.com>
2022-11-07 14:25:28 -0800

    11:17 test

--- src/Brown_Marshall_Q3.java
+++ src/Brown_Marshall_Q3.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Brown_Marshall_Q3 {
     public static void main(String[] args) {
-        int days = 0;
         int input = 0;
-        float dailyRate, medCharges, serviceCharges;
+        float dailyRate = 1, medCharges, serviceCharges, days = 1;
         Scanner kbd = new Scanner(System.in);
 
         System.out.println("Was the patient an:\n1) in-patient? \n2) out-patient?");
@@ -15,17 +14,40 @@ public class Brown_Marshall_Q3 {
             input = kbd.nextInt();
         }
 
-        if(input == 1) {
-            System.out.print("Please enter the number of days spent in hospital: ");
-            dailyRate = kbd.nextInt();
-            while(days < 0) {
-                System.out.println("please enter a valid input: ");
-                days = kbd.nextInt();
+        if(input == 1){
+            days = userInput("Please enter the number of days spent in hospital: ");
+            dailyRate = userInput("Please enter the daily rate: $");
             }
+        medCharges = userInput("Please enter the hospital medications charges: $");
+        serviceCharges = userInput("Please enter the charges for hospital services: $");
 
-            //figure out user input validation using method? ie userInput(String prompt)??
+        if(input ==1){
+            totalCharge(medCharges, serviceCharges, days, dailyRate);
+        }else{
+            totalCharge(medCharges, serviceCharges);
+        }
+    }
 
-            System.out.print("Please enter the daily rate:");
-        }
-        }
+    public static float userInput(String prompt){
+        float input;
+        Scanner kbd = new Scanner(System.in);
+        do {
+            System.out.print(prompt);
+            input = kbd.nextFloat();
+        }while(input<0);
+        return input;
+    }
+
+    public static float totalCharge(float a, float b, float c, float d){
+//        medCharges = userInput("Please enter the hospital medications charges: $");
+//        serviceCharges = userInput("Please enter the charges for hospital services: $");
+//        days = userInput("Please enter the number of days spent in hospital: ");
+//        dailyRate = userInput("Please enter the daily rate: $");
+        return(a+b+(c*d));
+    }
+
+    public static float totalCharge(float a, float b){
+        return(a+b);
+    }
+
 }
