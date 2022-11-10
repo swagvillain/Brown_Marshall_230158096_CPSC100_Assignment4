32150ea83eafa0b2e460da19c01a70e6061b9fa5
swagvillain <marshall.taylor.brown@gmail.com>
2022-11-09 17:49:21 -0800

    nearly done

--- src/Brown_Marshall_Q3.java
+++ src/Brown_Marshall_Q3.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Brown_Marshall_Q3 {
     public static void main(String[] args) {
-        int input = 0;
+        int input;
         float dailyRate = 1, medCharges, serviceCharges, days = 1;
         Scanner kbd = new Scanner(System.in);
 
-        System.out.println("Was the patient an:\n1) in-patient? \n2) out-patient?");
+        System.out.print("Was the patient:\n1) an in-patient? OR\n2) an out-patient?   ");
         input = kbd.nextInt();
 
         while(input != 1 && input != 2){
@@ -15,16 +15,19 @@ public class Brown_Marshall_Q3 {
         }
 
         if(input == 1){
-            days = userInput("Please enter the number of days spent in hospital: ");
+            days = userInput("You selected 1) an in-patient.\nPlease enter the number of days spent in hospital: ");
             dailyRate = userInput("Please enter the daily rate: $");
-            }
+            }else{
+            System.out.println("You selected 2) an out-patient.");
+        }
+
         medCharges = userInput("Please enter the hospital medications charges: $");
         serviceCharges = userInput("Please enter the charges for hospital services: $");
 
-        if(input ==1){
-            totalCharge(medCharges, serviceCharges, days, dailyRate);
-        }else{
-            totalCharge(medCharges, serviceCharges);
+        if(input == 1){
+            System.out.printf("Your total charge is: $%.2f", totalCharge(medCharges, serviceCharges, days, dailyRate));
+            }else{
+            System.out.printf("Your total charge is: $%.2f", totalCharge(medCharges, serviceCharges));
         }
     }
 
@@ -39,15 +42,22 @@ public class Brown_Marshall_Q3 {
     }
 
     public static float totalCharge(float a, float b, float c, float d){
-//        medCharges = userInput("Please enter the hospital medications charges: $");
-//        serviceCharges = userInput("Please enter the charges for hospital services: $");
-//        days = userInput("Please enter the number of days spent in hospital: ");
-//        dailyRate = userInput("Please enter the daily rate: $");
         return(a+b+(c*d));
     }
 
     public static float totalCharge(float a, float b){
         return(a+b);
     }
-
 }
+/*
+/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -javaagent:/opt/idea-IC-211.7628.21/lib/idea_rt.jar=35609:/opt/idea-IC-211.7628.21/bin -Dfile.encoding=UTF-8 -classpath /home/brownm6/Desktop/Marshall/CPSC100 Assignments/Brown_Marshall_230158096_CPSC100_Assignment4/out/production/Brown_Marshall_230158096_CPSC100_Assignment4 Brown_Marshall_Q3
+Was the patient:
+1) an in-patient? OR
+2) an out-patient?   1
+You selected 1) an in-patient.Please enter the number of days spent in hospital: 5
+Please enter the daily rate: $100
+Please enter the hospital medications charges: $45.60
+Please enter the charges for hospital services: $54.40
+Your total charge is: $600.00
+Process finished with exit code 0
+*/
